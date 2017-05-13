e429c0136af949fac910291490c95d2aad86329e
shaneknows <shanelknowles@gmail.com>
2017-05-13 11:19:14 -0400

    Fixing styles

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 A React Application for rendering a Unity3d game on github pages.
 
-To port your existing React Application [Follow these steps](https://medium.freecodecamp.com/surge-vs-github-pages-deploying-a-create-react-app-project-c0ecbf317089)
-
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
+import Head from "./layout/head";
 import './App.css';
 
 class App extends Component {
   render() {
+    const gameContainerStyle = {
+      width: "655px",
+      height: "400px"
+    };    
     return (
       <div className="App">
+        <Head />        
+        <div className="container content webgl-content">
+          <div id="gameContainer" style={gameContainerStyle}></div>
+          <div className="footer">
+            <div className="webgl-logo"></div>
+            <div className="fullscreen" onClick={this.triggerFullscreen}></div>
+            <div className="title">Naifu</div>
+          </div>
+        </div>
       </div>
     );
+  }
+
+  triggerFullscreen (e) {
+    document.getElementById("fs-link").click();
   }
 }
 
